b3fd2f7ebd5e72f0451ca6790c62ebec54d3cb46
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-09-19 17:20:12 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,42 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
-test('testando App', () => {
-  renderWithRouter(<App />);
+describe('testa os links', () => {
+  it('testando nomes', () => {
+    renderWithRouter(<App />);
 
-  const home = screen.getByText('Home');
+    const home = screen.getByText('Home');
+    const about = screen.getByText('About');
+    const favorite = screen.getByText('Favorite Pokémons');
 
-  expect(home).toBeInTheDocument();
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+  });
+
+  it('testa rota "Home"', () => {
+    const { history } = renderWithRouter(<App />);
+    const roleHome = screen.getByRole('link', { name: /Home/i });
+    userEvent.click(roleHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('testa rota "About"', () => {
+    const { history } = renderWithRouter(<App />);
+    const roleAbout = screen.getByRole('link', { name: /about/i });
+    userEvent.click(roleAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('testa rota "Favorite"', () => {
+    const { history } = renderWithRouter(<App />);
+    const roleFavorite = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(roleFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
 });

49a2b1136dca7fc289777959aa02cf4e9b91d183
Guiotek <guilherme.fagundes.016@gmail.com>
2022-09-19 16:08:51 -0300

    initial test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('testando App', () => {
+  renderWithRouter(<App />);
+
+  const home = screen.getByText('Home');
+
+  expect(home).toBeInTheDocument();
+});

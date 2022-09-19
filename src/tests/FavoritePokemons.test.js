0f2130a4214ff5b3f5764960f744b7b466e2e44c
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-09-19 18:12:32 -0300

    req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('testa <FavoritePokemons/>', () => {
+  it('testa se aparece o texto', () => {
+    render(<FavoritePokemons />);
+
+    const texto = screen.getByText('No favorite pokemon found');
+    console.log(texto);
+
+    expect(texto).toBeInTheDocument();
+  });
+});

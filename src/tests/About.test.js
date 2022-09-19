9a3ec1eb06d6c560c3ed7c28a6d60654607aaca2
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-09-19 17:55:12 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import About from '../pages/About';
+
+describe('testa <About/>', () => {
+  it('test se existe o texto "About Pokédex"', () => {
+    render(<About />);
+
+    const text = screen.getByText('About Pokédex');
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('', () => {
+    render(<About />);
+
+    const img = screen.getByRole('img', { name: /pokédex/i });
+    console.log(img);
+    expect(img).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});

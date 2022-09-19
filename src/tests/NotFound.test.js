428c72f3d322f2db69014c846505bb4d40ff92b0
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-09-19 18:21:28 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('testa <About/>', () => {
+  it('test se existe o texto "About Pokédex"', () => {
+    render(<NotFound />);
+
+    const text = screen.getByText('Page requested not found');
+
+    expect(text).toBeInTheDocument();
+  });
+
+  it('', () => {
+    render(<NotFound />);
+
+    const img = screen.getByRole(
+      'img',
+      { name: /pikachu crying because the page requested was not found/i },
+    );
+    console.log(img);
+    expect(img).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});

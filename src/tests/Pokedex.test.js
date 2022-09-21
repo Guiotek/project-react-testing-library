44ddcd94d2f32ee6eba7f00fc502bafb10d4132c
Guilhemr fagundes <gulherme.fagundes.016@gmail.com~>
2022-09-21 16:58:32 -0300

    req 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,6 +1,8 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import { filteredPokemons } from '../pages/Pokedex';
 
 describe('testa "<Pokedex />"', () => {
   it('testa nome', async () => {
@@ -10,13 +12,31 @@ describe('testa "<Pokedex />"', () => {
 
     expect(text).toBeInTheDocument();
   });
-  it('testa botoes pokemon', async () => {
+  it('', () => {
     renderWithRouter(<App />);
 
-    const button1 = screen.getByRole('button', {
-      name: /electric/i,
-    });
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    const a = 7;
 
-    expect(button1).toHaveProperty();
+    expect(buttons.length).toBe(a);
+  });
+  it('', () => {
+    renderWithRouter(<App />);
+
+    const t1 = screen.getByRole('button', { name: /electric/i });
+    const t2 = screen.getByRole('button', { name: /fire/i });
+    const t3 = screen.getByRole('button', { name: /bug/i });
+    const t4 = screen.getByRole('button', { name: /poison/i });
+    const t5 = screen.getByRole('button', { name: /psychic/i });
+    const t6 = screen.getByRole('button', { name: /normal/i });
+    const t7 = screen.getByRole('button', { name: /dragon/i });
+
+    expect(t1).toBeInTheDocument();
+    expect(t2).toBeInTheDocument();
+    expect(t3).toBeInTheDocument();
+    expect(t4).toBeInTheDocument();
+    expect(t5).toBeInTheDocument();
+    expect(t6).toBeInTheDocument();
+    expect(t7).toBeInTheDocument();
   });
 });
